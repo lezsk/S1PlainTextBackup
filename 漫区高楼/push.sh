6a99fbd6b33dbd17f97eaf459851d7a2b2b5a1cd
ozen <trembleafterme@outlook.com>
2020-02-05 10:10:13 +0800

    2020-02-05-10:10

--- 漫区高楼/push.sh
+++ 漫区高楼/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 datime=$(date "+%Y-%m-%d-%H:%M")
 git add .
-git commit -m "update at : $datime"
+git commit -m "$datime"
 echo "git commit: $datime"
 git push origin master
 echo "finished..."
