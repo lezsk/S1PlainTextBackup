fb1aa04c01808c515a3d33f76240dc6d43cbaa54
ozen <trembleafterme@outlook.com>
2020-02-07 11:26:08 +0800

    上传于2020-02-07-11:26

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 datime=$(date "+%Y-%m-%d-%H:%M")
 git add .
-git commit -m "更新于$datime"
-echo "git commit: 更新于$datime"
+git commit -m "上传于$datime"
+echo "git commit: 上传于$datime"
 git push origin master
 echo "finished..."
